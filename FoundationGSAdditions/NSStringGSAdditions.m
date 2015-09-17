f45822a1b6b148fa0406bf2ddc67a5f707ffb0fe
Goichi Hirakawa <gooichi@gyazsquare.com>
2015-09-17 19:45:23 +0900

    refs #17 Support watchOS

--- FoundationGSAdditions/NSStringGSAdditions.m
+++ FoundationGSAdditions/NSStringGSAdditions.m
@@ -21,7 +21,7 @@
     return (aString && ([self rangeOfString:aString options:(NSCaseInsensitiveSearch | NSBackwardsSearch | NSAnchoredSearch)].length > 0));
 }
 
-#if MAC_OS_X_VERSION_10_5 <= MAC_OS_X_VERSION_MAX_ALLOWED || __IPHONE_2_0 <=  __IPHONE_OS_VERSION_MAX_ALLOWED
+#if MAC_OS_X_VERSION_10_5 <= MAC_OS_X_VERSION_MAX_ALLOWED || __IPHONE_2_0 <= __IPHONE_OS_VERSION_MAX_ALLOWED || __WATCHOS_2_0 <= __WATCH_OS_VERSION_MAX_ALLOWED
 - (CGFloat)gs_cgFloatValue {
     if (sizeof(CGFloat) > sizeof(float)) {
         return [self doubleValue];
